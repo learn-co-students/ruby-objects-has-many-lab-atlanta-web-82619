395bf88c5981df957257bf5b9bfe290cccf0615f
Alexandria <alex.pugia816@hotmail.com>
2019-08-29 17:53:46 -0400

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,24 @@
+class Author
+attr_accessor :name, :posts
+
+    def initialize(name)
+        @name = name
+    end
+
+    def posts
+        Post.all.select {|p| p.author == self}
+    end
+
+    def add_post(post)
+        post.author = self
+    end
+
+    def add_post_by_title(post_title)
+        p = Post.new(post_title)
+        p.author = self
+    end
+
+    def self.post_count
+        Post.all.count
+    end
+end
